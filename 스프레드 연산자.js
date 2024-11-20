2ef23d9e8fec54fea34a1f2b5674ef9d900d6616
ganghyeon-Tak <76550689+ganghyeon-Tak@users.noreply.github.com>
2024-11-20 23:11:02 +0900

    Update 스프레드 연산자.js

--- 스프레드 연산자.js
+++ 스프레드 연산자.js
@@ -10,6 +10,8 @@ const oldArrAdd = emergencyFood.concat(emergencyFood2);
 console.log(`배열1 : ${emergencyFood}`);
 console.log(`배열2 : ${emergencyFood2}`);
 console.log(`예전 배열 병합 : ${oldArrAdd}`);
+// ['chocolet', 'candy', 'bisket', 'hardtack', 'almond'];
+
 
 // ES6 이후 spread 연산자 활용법 
 const newArrAdd = [...emergencyFood, ...emergencyFood2];
@@ -17,6 +19,7 @@ const newArrAdd = [...emergencyFood, ...emergencyFood2];
 console.log(`배열1 : ${emergencyFood}`);
 console.log(`배열2 : ${emergencyFood2}`);
 console.log(`스프레드 연산자 사용 : ${newArrAdd}`);
+// ['chocolet', 'candy', 'bisket', 'hardtack', 'almond']
 
 // 스프레드 연산자는 객체에서도 사용 가능하다.
 // 객체 안에 키와 값을 추가하고 싶을 때도 사용가능하다.
@@ -26,3 +29,4 @@ let user = { name: 'gang', city: 'pohang'};
 user = {...user, gender: 'male'};
 
 console.log(user);
+// { name: 'gang', city: 'pohang', gender: 'male'};
