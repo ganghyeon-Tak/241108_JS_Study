3872fdd17b57766db499a1b15b99e22b64a8e4c9
ganghyeon-Tak <76550689+ganghyeon-Tak@users.noreply.github.com>
2024-11-20 21:01:46 +0900

    Update 스프레드 연산자.js

--- 스프레드 연산자.js
+++ 스프레드 연산자.js
@@ -7,9 +7,13 @@ const emergencyFood2 = ['bisket', 'hardtack', 'almond'];
 // 기존 배열 병합
 const oldArrAdd = emergencyFood.concat(emergencyFood2);
 
-console.log(oldArrAdd);
+console.log(`배열1 : ${emergencyFood}`);
+console.log(`배열2 : ${emergencyFood2}`);
+console.log(`예전 배열 병합 : ${oldArrAdd}`);
 
 // ES6 이후 spread 연산자 활용법
 const newArrAdd = [...emergencyFood, ...emergencyFood2];
 
-console.log(newArrAdd);
+console.log(`배열1 : ${emergencyFood}`);
+console.log(`배열2 : ${emergencyFood2}`);
+console.log(`스프레드 연산자 사용 : ${newArrAdd}`);
