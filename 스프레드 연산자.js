513dbe6ef715aafcee7e49af5c1ae645399e8a99
ganghyeon-Tak <76550689+ganghyeon-Tak@users.noreply.github.com>
2024-11-20 20:55:34 +0900

    Update 스프레드 연산자.js

--- 스프레드 연산자.js
+++ 스프레드 연산자.js
@@ -1,1 +1,15 @@
 // 두 개 이상의 배열을 하나로 합칠 때 사용하는 연산자
+
+// 테스트용 배열 선언
+const emergencyFood = ['chocolet', 'candy'];
+const emergencyFood2 = ['bisket', 'hardtack', 'almond'];
+
+// 기존 배열 병합
+const oldArrAdd = emergencyFood.concat(emergencyFood2);
+
+console.log(oldArrAdd);
+
+// ES6 이후 spread 연산자 활용법
+const newArrAdd = [...emergencyFood, ...emergencyFood2];
+
+console.log(newArrAdd);
